20e962769beb8ddfafd32824e0b47403cf933fbb
Manraj Singh <manrajsinghgrover@gmail.com>
2016-10-11 23:24:12 +0530

    Adds few comments

--- v1/index.php
+++ v1/index.php
@@ -2,25 +2,65 @@
   use \Psr\Http\Message\ServerRequestInterface as Request;
   use \Psr\Http\Message\ResponseInterface as Response;
 
+  /**
+   * Load dependencies and includes
+   */
   require_once '../vendor/autoload.php';
   require_once '../includes/DbAccess.php';
   require_once '../includes/functions.php';
 
+  /**
+   * Slim App instance
+   * @var Object
+   */
   $app = new \Slim\App;
 
+  /**
+   * Matches route to /doctors and gets details of all doctors from the database
+   * in paginated format.
+   * @optional page   Gets the details of doctors on that page. Defaults to 1
+   * @return JSON     Details of all doctors on particular page
+   */
   $app->get('/doctors', function (Request $request, Response $response) use ($app) {
     try {
+      /**
+       * Create Database instance
+       * @var Object Database Access Class
+       */
       $db = new DbAccess('localhost', '8889', 'root', 'root', 'healthcare');
+
+      /**
+       * Connect to Database
+       * @var Object
+       */
       $db_connect = $db->connect();
 
+      /**
+       * Get all query parameters
+       */
       $allParams = $request->getQueryParams();
 
+      /**
+       * Get offset based on page number
+       */
       $params = getOffset($allParams);
 
+      /**
+       * Prepare query for getting details of doctors on that page
+       * @var String
+       */
       $prepare_query = "SELECT * FROM doctors LIMIT :limit OFFSET :offset;";
       $query = $db_connect->prepare($prepare_query);
+
+      /**
+       * Query the database and get rows of each doctor
+       * @var Array
+       */
       $data = $db->query($query, $params);
 
+      /**
+       * Check if any doctor exist on the page
+       */
       if (count($data) === 0) {
         $arrResponse = array('error' => true, 'message' => 'No more pages exist');
       } else {
@@ -30,21 +70,59 @@
     } catch(PDOException $Exception) {
       $arrResponse = array('error' => true, 'message' => 'Server is unable to get data');
     }
+
+    /**
+     * Send JSON as response
+     */
     return $response->withJson($arrResponse);
   });
 
+  /**
+   * Matches route to /clinics and gets details of all clinics from the database
+   * in paginated format.
+   * @optional page   Gets the details of clinics on that page. Defaults to 1
+   * @return JSON     Details of all clinics on particular page
+   */
   $app->get('/clinics', function (Request $request, Response $response) use ($app) {
     try {
+      /**
+       * Create Database instance
+       * @var Object Database Access Class
+       */
       $db = new DbAccess('localhost', '8889', 'root', 'root', 'healthcare');
+
+      /**
+       * Connect to Database
+       * @var Object
+       */
       $db_connect = $db->connect();
 
+      /**
+       * Get all query parameters
+       */
       $allParams = $request->getQueryParams();
 
+      /**
+       * Get offset based on page number
+       */
       $params = getOffset($allParams);
+
+      /**
+       * Prepare query for getting details of clinics on that page
+       * @var String
+       */
       $prepare_query = "SELECT * FROM clinics LIMIT :limit OFFSET :offset;";
       $query = $db_connect->prepare($prepare_query);
+
+      /**
+       * Query the database and get rows of each clinic
+       * @var Array
+       */
       $data = $db->query($query, $params);
 
+      /**
+       * Check if any doctor exist on the page
+       */
       if(count($data) === 0) {
         $arrResponse = array('error' => true, 'message' => 'No more pages exist');
       } else {
@@ -54,6 +132,10 @@
     } catch(PDOException $Exception) {
       $arrResponse = array('error' => true, 'message' => 'Server is unable to get data');
     }
+    
+    /**
+     * Send JSON as response
+     */
     return $response->withJson($arrResponse);
   });
 
