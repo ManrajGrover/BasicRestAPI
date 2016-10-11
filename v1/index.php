eec8f1687de401ef540a019bf2a213e00d3607dd
Manraj Singh <manrajsinghgrover@gmail.com>
2016-10-11 18:47:58 +0530

    DbAccess Added and imported

--- v1/index.php
+++ v1/index.php
@@ -2,14 +2,34 @@
   use \Psr\Http\Message\ServerRequestInterface as Request;
   use \Psr\Http\Message\ResponseInterface as Response;
 
-  require '../vendor/autoload.php';
+  require_once '../vendor/autoload.php';
+  require_once '../includes/DbAccess.php';
 
   $app = new \Slim\App;
-  $app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
 
+  $app->get('/doctors', 'getAllDoctors');
+  $app->get('/clinics', 'getAllClinics');
+  $app->get('/doctors/{id}', 'getDoctorDetails');
+  $app->get('/clinics/{id}', 'getClinicsDetails');
+
+  function getAllDoctors (Request $request, Response $response) {
+    $db_connect = new DbAccess('localhost', '8889', 'root', 'root', 'healthcare');
+    $data = array('name' => 'Bob', 'age' => 40);
+    $response = $response->withJson($data);
     return $response;
-  });
+  }
+
+  function getAllClinics(Request $request, Response $response) {
+
+  }
+
+  function getDoctorDetails (Request $request, Response $response) {
+    $doc_id = $request->getAttribute('id');
+  }
+
+  function getClinicsDetails (Request $request, Response $response) {
+    $clinic_id = $request->getAttribute('id');
+  }
+
   $app->run();
 ?>
